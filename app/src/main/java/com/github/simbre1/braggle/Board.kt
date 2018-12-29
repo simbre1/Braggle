ace5926a541ca8c2dd645775c7f2869f790cca39
simbre1 <stijn.imbrechts+github@gmail.com>
2018-12-29 22:07:21 +0100

    dice letter distribution

--- app/src/main/java/com/github/simbre1/braggle/Board.kt
+++ app/src/main/java/com/github/simbre1/braggle/Board.kt
@@ -12,19 +12,92 @@ class Board(private var letters: Array<Array<Char>>) {
 
     companion object Factory {
 
-        private val defaultCharset = arrayListOf(
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+        fun random(size: Int) : Board {
+            return random(
+                size,
+                Random().nextLong(),
+                english16)
+        }
 
         fun random(size: Int,
-                   seed: Long) : Board {
+                   seed: Long,
+                   dice: Array<String>): Board {
+            if (dice.isEmpty()) {
+                throw IllegalArgumentException("no dice")
+            }
+
             val rand = Random(seed)
-            var letters = Array(size) { Array(size) { defaultCharset[rand.nextInt(defaultCharset.size)] } }
+            val letters = Array(size) { i ->
+                Array(size) { j ->
+                    val diceNr = ((i*size) + j) % dice.size
+                    dice[diceNr][rand.nextInt(6)]
+                }
+            }
+
             return Board(letters)
         }
 
-        fun random(size: Int) : Board {
-            return random(size, Random().nextLong())
-        }
+        private val english16 = arrayOf(
+            "AAEEGN",
+            "ABBJOO",
+            "ACHOPS",
+            "AFFKPS",
+            "AOOTTW",
+            "CIMOTU",
+            "DEILRX",
+            "DELRVY",
+            "DISTTY",
+            "EEGHNW",
+            "EEINSU",
+            "EHRTVW",
+            "EIOSST",
+            "ELRTTY",
+            "HIMNUQ",
+            "HLNNRZ")
+
+//        private val french16 = arrayOf(
+//            "ETUKNO",
+//            "EVGTIN",
+//            "DECAMP",
+//            "IELRUW",
+//            "EHIFSE",
+//            "RECALS",
+//            "ENTDOS",
+//            "OFXRIA",
+//            "NAVEDZ",
+//            "EIOATA",
+//            "GLENYU",
+//            "BMAQJO",
+//            "TLIBRA",
+//            "SPULTE",
+//            "AIMSOR",
+//            "ENHRIS")
+//
+//        private val english25 = arrayOf(
+//            "AAAFRS",
+//            "AAEEEE",
+//            "AAFIRS",
+//            "ADENNN",
+//            "AEEEEM",
+//            "AEEGMU",
+//            "AEGMNN",
+//            "AFIRSY",
+//            "BBJKXZ",
+//            "CCENST",
+//            "EIILST",
+//            "CEIPST",
+//            "DDHNOT",
+//            "DHHLOR",
+//            "DHHNOW",
+//            "DHLNOR",
+//            "EIIITT",
+//            "EILPST",
+//            "EMOTTT",
+//            "ENSSSU",
+//            "FIPRSY",
+//            "GORRVW",
+//            "IPRSYY",
+//            "NOOTUW",
+//            "OOOTTU")
     }
 }
